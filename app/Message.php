adbc5fdfd1bf72c4d05d8224b7867ba4574ac046
Julien <julien@Debian-Preprod.gpmaurizi.local>
2018-01-04 10:58:03 +0100

    Add helper
    Signed-off-by: Julien <julien@Debian-Preprod.gpmaurizi.local>

--- app/Message.php
+++ app/Message.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $table = 'messages';
+
+    public function whereSensorIs(Sensor $sensor)
+    {
+        return $this->where('node_address', '=', $sensor->node_address)->where('sensor_address', '=', $sensor->sensor_address);
+    }
 }
