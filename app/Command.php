66b1033a8432eb8054270ef4796b86fed5ff55ce
Julien <julien@Debian-Preprod.gpmaurizi.local>
2020-10-12 17:29:21 +0200

    Mass assignement command

--- app/Command.php
+++ app/Command.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Command extends Model
 {
+
+    protected $guarded = ['created_at', 'updated_at'];
     public function commandable()
     {
         return $this->morphTo();
