8d255d28f9c683d27f86b3c7ce856ecb367b889f
Julien Lamandin <julien.lamandin@gmail.com>
2020-10-16 19:48:16 +0200

    Update spotify library

--- app/Http/Controllers/SpotifyController.php
+++ app/Http/Controllers/SpotifyController.php
@@ -28,7 +28,7 @@ class SpotifyController extends Controller
                 'user-read-recently-played',
             ],
         ];
-        header('Location: '.$session->getAuthorizeUrl($options));
+        header('Location: ' . $session->getAuthorizeUrl($options));
         die();
     }
 
@@ -43,8 +43,7 @@ class SpotifyController extends Controller
         $accessToken = $session->getAccessToken();
         $refreshToken = $session->getRefreshToken();
         $config = SpotifyConfig::first();
-        if(!$config)
-        {
+        if (!$config) {
             $config = new SpotifyConfig();
         }
         $config->access_token = $accessToken;
@@ -57,51 +56,17 @@ class SpotifyController extends Controller
 
     public function test(Request $request)
     {
-        //        //dd(route('spotify'));
-        //        $api = new \SpotifyWebAPI\SpotifyWebAPI();
-        //
-        //        // Fetch the saved access token from somewhere. A database for example.
-        //        $api->setAccessToken($request->session()->get('access_token'));
-        //
-        //        // It's now possible to request data about the currently authenticated user
-        //
-        //
-        //        //dd($api->search('sexual healing marvin gaye', ['track']));
-        //        //3VZmChrnVW8JK6ano4gSED
-        //        dd($api->getMyDevices());
-        //        //d622989173578e1a242dc0f236c293259d021b94
-        //        //dd($api->play('d622989173578e1a242dc0f236c293259d021b94', ['uri
-        //s' => ["spotify:track:3VZmChrnVW8JK6ano4gSED"]]));
-        //        //dd($api->pause());
-        //        //dd($api->play('d622989173578e1a242dc0f236c293259d021b94'));
-        //        //dd($api->getMyDevices());
-        $command = SpotifyCommand::find(3);
-        dd($command->play());
+
     }
 
     public function config(Request $request)
     {
-        //        $session = new \SpotifyWebAPI\Session(
-        //            config('services.spotify.client_id'),
-        //            config('services.spotify.client_secret'),
-        //            route('spotify_ok')
-        //        );
-        //        $session->refreshAccessToken($request->session()->get('refresh_token'));
-        //        $accessToken = $session->getAccessToken();
-        //        $refreshToken = $session->getRefreshToken();
-        //
-        //        $request->session()->put('access_token', $accessToken);
-        //        $request->session()->put('refresh_token', $refreshToken);
         $spotify_config = SpotifyConfig::first();
-        try
-        {
+        try {
             $api = new \SpotifyWebAPI\SpotifyWebAPI();
             $api->setAccessToken($spotify_config->access_token);
-            $reponse = $api->getMyDevices();
-        }
-        catch (SpotifyWebAPIException $e)
-        {
-            if($e->getCode() == 401)//Access token too old
+        } catch (SpotifyWebAPIException $e) {
+            if ($e->getCode() == 401)//Access token too old
             {
                 $session = new \SpotifyWebAPI\Session(
                     config('services.spotify.client_id'),
@@ -138,13 +103,10 @@ class SpotifyController extends Controller
             'spotify_device_id' => 'required',
         ]);
         $spotify_config = SpotifyConfig::first();
-        if($spotify_config)
-        {
+        if ($spotify_config) {
             $spotify_config->device_id = $request->spotify_device_id;
             $spotify_config->save();
-        }
-        else
-        {
+        } else {
             $spotify_config = new SpotifyConfig();
             $spotify_config->device_id = $request->spotify_device_id;
             $spotify_config->save();
@@ -158,13 +120,10 @@ class SpotifyController extends Controller
             'spotify_track_id' => 'required',
         ]);
         $spotify_config = SpotifyConfig::first();
-        if($spotify_config)
-        {
+        if ($spotify_config) {
             $spotify_config->track_id = $request->spotify_track_id;
             $spotify_config->save();
-        }
-        else
-        {
+        } else {
             $spotify_config = new SpotifyConfig();
             $spotify_config->track_id = $request->spotify_track_id;
             $spotify_config->save();
@@ -214,7 +173,7 @@ class SpotifyController extends Controller
         $spotify_command->uri = $request->uri;
         $spotify_command->save();
         $command = new Command();
-        $command->name = ucfirst($request->type).' '.$request->name;
+        $command->name = ucfirst($request->type) . ' ' . $request->name;
         $spotify_command->command()->save($command);
         return redirect()->back();
     }
--- app/SpotifyCommand.php
+++ app/SpotifyCommand.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Interfaces\CommandInterface;
 use Illuminate\Database\Eloquent\Model;
+use SpotifyWebAPI\SpotifyWebAPI;
 
 class SpotifyCommand extends Model implements CommandInterface
 {
@@ -14,35 +15,27 @@ class SpotifyCommand extends Model implements CommandInterface
 
     public function play()
     {
+        /***AUTH***/
         $spotify_config = SpotifyConfig::first();
-        try
-        {
-            $api = new \SpotifyWebAPI\SpotifyWebAPI();
-            $api->setAccessToken($spotify_config->access_token);
-            $reponse = $api->getMyDevices();
-        }
-        catch (SpotifyWebAPIException $e)
-        {
-            if($e->getCode() == 401)//Access token too old
-            {
-                $session = new \SpotifyWebAPI\Session(
-                    config('services.spotify.client_id'),
-                    config('services.spotify.client_secret'),
-                    route('spotify_ok')
-                );
-                $session->refreshAccessToken($spotify_config->refresh_token);
-                $accessToken = $session->getAccessToken();
-                $refreshToken = $session->getRefreshToken();
-                $spotify_config->access_token = $accessToken;
-                $spotify_config->refresh_token = $refreshToken;
-                $spotify_config->save();
-            }
-        }
+        $session = new \SpotifyWebAPI\Session(
+            config('services.spotify.client_id'),
+            config('services.spotify.client_secret'),
+            route('spotify_ok')
+        );
+        $session->refreshAccessToken($spotify_config->refresh_token);
+        $accessToken = $session->getAccessToken();
+        $refreshToken = $session->getRefreshToken();
+        $spotify_config->access_token = $accessToken;
+        $spotify_config->refresh_token = $refreshToken;
+        $spotify_config->save();
+        $api = new \SpotifyWebAPI\SpotifyWebAPI();
         $api->setAccessToken($spotify_config->access_token);
-        if($this->type == 'playlist')
+        /**********/
+        $spotify_config->access_token = $accessToken;
+        $spotify_config->save();
+        if ($this->type == 'playlist')
             $api->play($spotify_config->device_id, ['context_uri' => $this->uri]);
-        else if($this->type == 'track')
+        else if ($this->type == 'track')
             $api->play($spotify_config->device_id, ['uris' => [$this->uri]]);
-
     }
 }
